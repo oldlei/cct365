8a42caf086616436916f08afdcda3f4d5d7588d1
oldlei <814084188@qq.com>
2017-02-03 16:36:31 +0800

    多用户登录

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['prefix' => 'admin','namespace' => 'Admin'],function ($router)
+{
+    $router->get('login', 'LoginController@showLoginForm');
+    $router->post('login', 'LoginController@login');
+    $router->post('logout', 'LoginController@logout');
+
+    $router->get('index', 'IndexController@index');
+		$router->get('/', 'IndexController@index');
+});
